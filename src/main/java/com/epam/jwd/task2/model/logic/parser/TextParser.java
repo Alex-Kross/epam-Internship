a10ae35e443285f8ef5ff36e9e06fdc39b329838
Alex-Kross <artemkirlanovia@gmail.com>
2022-04-17 23:50:15 +0300

    add opportunity for delete last line brake after parsing

--- src/main/java/com/epam/jwd/task2/model/logic/parser/TextParser.java
+++ src/main/java/com/epam/jwd/task2/model/logic/parser/TextParser.java
@@ -2,13 +2,20 @@ package com.epam.jwd.task2.model.logic.parser;
 
 import com.epam.jwd.task2.model.entity.CompositeTextElement;
 import com.epam.jwd.task2.model.entity.RegEx;
+import com.epam.jwd.task2.model.entity.composite.Text;
+import com.epam.jwd.task2.util.RemoverLastLineBrake;
 
 public class TextParser {
-    public static void parseText(CompositeTextElement text, String sourceText){
+    public static void parseText(CompositeTextElement textElement, String textFromSourceFile){
         TextElementParser parser = getParserChain();
-        String[] strings = sourceText.split(RegEx.LINE_BRAKE.getRegEx());
-        for (String string : strings) {
-            parser.parse(string, text);
+        String[] listStringOfText = textFromSourceFile.split(RegEx.LINE_BRAKE.getRegEx());
+
+        for (int i = 0; i < listStringOfText.length; i++) {
+            parser.parse(listStringOfText[i], textElement);
+            if (i+1 >= listStringOfText.length){
+                // remove last line brake of the end int the text
+                RemoverLastLineBrake.remove((Text) textElement);
+            }
         }
     }
 
---
+++ src/main/java/com/epam/jwd/task2/util/RemoverLastLineBrake.java
@@ -0,0 +1,54 @@
+package com.epam.jwd.task2.util;
+
+import com.epam.jwd.task2.model.entity.TextElement;
+import com.epam.jwd.task2.model.entity.composite.Paragraph;
+import com.epam.jwd.task2.model.entity.composite.Sentence;
+import com.epam.jwd.task2.model.entity.composite.Text;
+import com.epam.jwd.task2.model.entity.leaf.CodeBlock;
+
+import java.util.List;
+
+/**
+ * Class remove last line brake in text
+ *
+ * @author Karpuk A.U.
+ * @version 1.1 20.2.2022
+ * */
+public class RemoverLastLineBrake {
+    /**
+     * Remove last line brake in the end of textElement
+     * Retrieve last paragraph from Text then retrieve last sentences or
+     * code block from paragraph.
+     * If we get a sentence then from list words and
+     * punctuation marks delete last punct mark that is a
+     * line brake.
+     * If we get a codeBlock then trim string of
+     * code block on 1 character from the end
+     *
+     * @param textElement object that contain text
+     */
+    public static void remove(Text textElement){
+        if (textElement == null) {
+            throw new NullPointerException("Text element is null");
+        }
+        // get last paragraph
+        int indexLastParagraph = textElement.getParagraphs().size() - 1;
+        TextElement lastParagraph = textElement.getParagraphs().get(indexLastParagraph);
+
+        // get list sentence or block code from last paragraph
+        List<TextElement> listSentenceOrCode = ((Paragraph) lastParagraph).getSentencesAndCode();
+
+        // get last sentence or code block
+        int indexLastSentenceOrCode = listSentenceOrCode.size() - 1;
+        TextElement sentenceOrCode = listSentenceOrCode.get(indexLastSentenceOrCode);
+
+        if (sentenceOrCode.getClass() == Sentence.class) {
+            List<TextElement> listWordsOrPunctMarks = ((Sentence) sentenceOrCode).getWordsAndPunctMarks();
+            listWordsOrPunctMarks.remove(listWordsOrPunctMarks.size()-1);
+        }
+        else if(sentenceOrCode.getClass() == CodeBlock.class){
+            StringBuilder codeBlock = ((CodeBlock) sentenceOrCode).getCode();
+            codeBlock.deleteCharAt(codeBlock.length()-1);
+        }
+    }
+}
